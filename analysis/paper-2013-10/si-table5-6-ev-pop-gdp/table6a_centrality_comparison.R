b1d9dc2c8f2368847c39cf112294b3ebddad39e0
Shahar Ronen <shahar.ronen@gmail.com>
2014-03-01 19:56:57 -0800

    comparison of cent. measures

---
+++ analysis/paper-2013-10/si-table5-6-ev-pop-gdp/table6a_centrality_comparison.R
@@ -0,0 +1,98 @@
+# Code for generating SI Table 5: GDP+Pop per language, 
+# and SI table 6: Language EV centrality
+
+source("../load.R", chdir=T)
+library(igraph)
+
+get.lgn.metrics <- function(file.in, src.name) {
+  # Table of LGN graph metrics for each language from given source
+  # A different minimum is used for books!
+  if (src.name!="book") {
+    filtered.edgelist <- read.filtered.edgelist(file.in, MIN.COMMON.USERS)
+  }
+  else {
+    filtered.edgelist <- read.filtered.edgelist(file.in, MIN.COMMON.TRANS)
+  }
+  #filtered.edgelist <<-filtered.edgelist 
+  
+  ## measure EV and betweenness centralities in three ways: (1) unweighted,
+  ## (2) exposure as weight, (3) num common as weight
+  
+  # unweighted
+  print(filtered.edgelist[1:2,])
+  lgn.graph <- graph.data.frame(filtered.edgelist, directed=TRUE)
+  lgn.metrics <- data.frame(
+    unw.eig=evcent(lgn.graph)$vector,
+    unw.bet=betweenness(lgn.graph)
+  )
+  
+  # weighted by exposure
+  names(filtered.edgelist)[names(filtered.edgelist) == 'exposure'] <- 'weight'
+  print(filtered.edgelist[1:2,])
+  lgn.graph <- graph.data.frame(filtered.edgelist, directed=TRUE)
+  lgn.metrics$exposure.eig <- evcent(lgn.graph)$vector
+  lgn.metrics$exposure.bet <- betweenness(lgn.graph)
+  
+  # weighted by common users/translations
+  names(filtered.edgelist)[names(filtered.edgelist) == 'weight'] <- 'exposure'
+  names(filtered.edgelist)[names(filtered.edgelist) == 'common.num'] <- 'weight'
+  print(filtered.edgelist[1:2,])
+  lgn.graph <- graph.data.frame(filtered.edgelist, directed=TRUE)
+  lgn.metrics$common.eig <- evcent(lgn.graph)$vector
+  lgn.metrics$common.bet <- betweenness(lgn.graph)
+ 
+  return(lgn.metrics)
+}
+
+get.cent.tables <- function(in.file, # langlang file to use
+                            out.file, # file to write to
+                            src.name, # twit, wiki, book - determines thresholds
+                            classif # language classification table file
+                            ) {
+  # Get all values
+  the.metrics <- get.lgn.metrics(in.file, src.name)
+  
+  # Add full lang names
+  output.table <- merge(the.metrics, classif[,c('Lang_Code','Lang_Name')], 
+                        all.x=T, # Keep value without a matching language name
+                        by.x="row.names", by.y="Lang_Code") 
+  # Rename column for future merging
+  colnames(output.table)[1] <- "Code"
+  
+  return(output.table)
+}
+
+# Get the full names of the languages and their number of speakers
+lang.classif.table <- read.csv(SPEAKER.STATS.FILE, sep="\t", header=T)
+
+##
+##
+#### CENTRALITY ####
+
+# Now get each language's cent vals
+twit.cent <- get.cent.tables(in.file=TWIT.STD.LANGLANG,
+                   src.name="twit", 
+                   classif=lang.classif.table)
+
+wiki.cent <- get.cent.tables(in.file=WIKI.STD.LANGLANG,
+                   src.name="wiki", 
+                   classif=lang.classif.table)
+
+book.cent  <- get.cent.tables(in.file=BOOKS.STD.LANGLANG,
+                   src.name="book", 
+                   classif=lang.classif.table)
+
+# Remove some languages and write
+twit.cent <- twit.cent[ ! twit.cent$Code %in% DISCARD.LANGS, ]
+write.table(twit.cent, file="table6a_cent_twit.tsv", 
+            sep="\t", quote=F, row.names=F, na="")
+
+wiki.cent <- wiki.cent[ ! wiki.cent$Code %in% DISCARD.LANGS, ]
+write.table(wiki.cent, file="table6a_cent_wiki.tsv", 
+            sep="\t", quote=F, row.names=F, na="")
+
+book.cent <- book.cent[ ! book.cent$Code %in% DISCARD.LANGS, ]
+write.table(book.cent, file="table6a_cent_book.tsv", 
+            sep="\t", quote=F, row.names=F, na="")
+
+print("TABLE 6 DONE - look for NA values")
