69435ac99e30c82c44798f488fee6d07447b458e
cnavarreteliz <cnavarreteliz@gmail.com>
2021-06-11 15:45:35 +0200

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  siteMetadata: {
+    title: "Global Language Network",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-plugin-google-analytics",
+      options: {
+        trackingId: "",
+      },
+    },
+    "gatsby-plugin-react-helmet",
+  ],
+};
